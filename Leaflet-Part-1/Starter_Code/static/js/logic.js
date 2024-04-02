40c19521728d901267a5c02b9e3fed8800e2c67e
Blake Stephenson <blake.stephenson@ymail.com>
2024-04-02 12:46:00 -0400

    creating circle markers

--- Leaflet-Part-1/Starter_Code/static/js/logic.js
+++ Leaflet-Part-1/Starter_Code/static/js/logic.js
@@ -4,51 +4,70 @@
 // Include popups that provide additional information about the earthquake when its associated marker is clicked.
 // Create a legend that will provide context for your map data.
 
-///////// Template for cirlce markers //////////////////////////////
+let queryurl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
 
-// A function to determine the marker size based on the population
-function markerSize(population) {
-    return Math.sqrt(population) * 50;
+d3.json(queryurl).then(function (data) {
+  createFeatures(data.features)
+});
+
+function createFeatures(EQdata) {
+  
+  function onEachFeature(feature, layer) {
+    layer.bindPopup('${features.properties.place}${new Date(features.properties.time)}'
+  )}
+
+  let EQ = L.geoJSON(EQdata, {
+    onEachFeature: onEachFeature
+  });
+
+  createMap(EQmap);
+}
+
+function createMap(EQmap) {
+
+  // Adding a tile layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(mymap);
+
+  function markerSize(mag) {
+    return mag;
   }
   
   // Define arrays to hold the created city and state markers.
-  let cityMarkers = [];
-  let stateMarkers = [];
+    let magMarkers = [];
+    //let depthMarkers = [];
   
-  // Loop through locations, and create the city and state markers.
-  for (let i = 0; i < locations.length; i++) {
+    // Loop through locations, and create the city and state markers.
+    for (let i = 0; i < EQdata.length; i++) {
     // Setting the marker radius for the state by passing population into the markerSize function
-    stateMarkers.push(
-      L.circle(locations[i].coordinates, {
-        stroke: false,
-        fillOpacity: 0.75,
-        color: "white",
-        fillColor: "white",
-        radius: markerSize(locations[i].state.population)
-      })
-    );
+      magMarkers.push(
+        L.circle(features[i].geometry.coordinates[0, 1], {
+          stroke: false,
+          fillOpacity: 0.75,
+          color: "white",
+          fillColor: "white",
+          radius: markerSize(features[i].properties.mag)
+        })
+    )};
   
     // Set the marker radius for the city by passing the population to the markerSize() function.
-    cityMarkers.push(
-      L.circle(locations[i].coordinates, {
-        stroke: false,
-        fillOpacity: 0.75,
-        color: "purple",
-        fillColor: "purple",
-        radius: markerSize(locations[i].city.population)
-      })
-    );
-  }
-///////// Template for cirlce markers //////////////////////////////
+    ////cityMarkers.push(
+      ////L.circle(locations[i].coordinates, {
+        ////stroke: false,
+        ////fillOpacity: 0.75,
+        ////color: "purple",
+        ////fillColor: "purple",
+        ////radius: markerSize(locations[i].city.population)
+      ////})
+    ////);
+  ////}
+  ///////// Template for cirlce markers //////////////////////////////
 
-// Create map
-let mymap = ("map", {
-    center: [,],
-    zoom: 
-});
-
-// Adding a tile layer
-L.tilelayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+  // Create map
+  let mymap = L.map("map", {
+      center: [37, -95],
+      zoom: 5
+  });
+}
 
