eab5b0f41d824324c2b2b45c4804b3c756c9b014
Blake Stephenson <blake.stephenson@ymail.com>
2024-04-02 18:23:48 -0400

    closer. started the map function

--- Leaflet-Part-1/Starter_Code/static/js/logic.js
+++ Leaflet-Part-1/Starter_Code/static/js/logic.js
@@ -4,70 +4,90 @@
 // Include popups that provide additional information about the earthquake when its associated marker is clicked.
 // Create a legend that will provide context for your map data.
 
-let queryurl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
-
-d3.json(queryurl).then(function (data) {
-  createFeatures(data.features)
-});
-
-function createFeatures(EQdata) {
-  
-  function onEachFeature(feature, layer) {
-    layer.bindPopup('${features.properties.place}${new Date(features.properties.time)}'
-  )}
-
-  let EQ = L.geoJSON(EQdata, {
-    onEachFeature: onEachFeature
+  // Create map
+  let mymap = L.map("map", {
+    center: [37, -95],
+    zoom: 35,
   });
 
-  createMap(EQmap);
-}
-
-function createMap(EQmap) {
-
   // Adding a tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  let streetlayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(mymap);
 
-  function markerSize(mag) {
-    return mag;
-  }
+  let queryurl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+
+  let mapstyle = {
+    color: "red",
+    fillcolor: "blue",
+    fillOpacity: 0.5,
+    weight: 1.5
+  };
+
+  d3.json(queryurl).then(function(data) {
+    L.geoJson(data, {
+      style: mapstyle
+    }).addTo(mymap);
+  });
+
+  //function createFeatures(EQdata) {
+  
+    //function onEachFeature(feature, layer) {
+      //layer.bindPopup(`${feature.properties.place}${new Date(feature.properties.time)}`);
+    //}
+  
+    //let EQ = L.geoJSON(EQdata, {
+      //onEachFeature: onEachFeature
+    //});
+  
+
+  //function markerSize(mag) {
+    //return mag;
+  //}
   
   // Define arrays to hold the created city and state markers.
-    let magMarkers = [];
+    //let magMarkers = [];
     //let depthMarkers = [];
   
     // Loop through locations, and create the city and state markers.
-    for (let i = 0; i < EQdata.length; i++) {
+    //for (let i = 0; i < EQ.length; i++) {
     // Setting the marker radius for the state by passing population into the markerSize function
-      magMarkers.push(
-        L.circle(features[i].geometry.coordinates[0, 1], {
-          stroke: false,
-          fillOpacity: 0.75,
-          color: "white",
-          fillColor: "white",
-          radius: markerSize(features[i].properties.mag)
-        })
-    )};
+      //magMarkers.push(
+        //L.circle(EQ[i].geometry.coordinates, {
+          //stroke: false,
+          //fillOpacity: 0.75,
+          //color: "white",
+          //fillColor: "red",
+          //radius: markerSize(EQ[i].properties.mag)
+        //})
+      //);
+    //}
   
     // Set the marker radius for the city by passing the population to the markerSize() function.
-    ////cityMarkers.push(
-      ////L.circle(locations[i].coordinates, {
-        ////stroke: false,
-        ////fillOpacity: 0.75,
-        ////color: "purple",
-        ////fillColor: "purple",
-        ////radius: markerSize(locations[i].city.population)
-      ////})
-    ////);
-  ////}
-  ///////// Template for cirlce markers //////////////////////////////
+      //depthMarkers.push(
+      //L.circle(EQ[i].geometry.coordinates, {
+        //stroke: false,
+        //fillOpacity: 0.75,
+        //color: "purple",
+        //fillColor: "purple",
+        //radius: markerSize(EQ[i].geometry.coordinates[2])
+      //})
+    //);
+  //}
 
-  // Create map
-  let mymap = L.map("map", {
-      center: [37, -95],
-      zoom: 5
-  });
-}
+  // Create two separate layer groups: one for the city markers and another for the state markers.
+  //let mag = L.layerGroup(magMarkers);
+  //let depth = L.layerGroup(depthMarkers);
 
+  //let overlaymaps = {
+    //"Magnitude": mag,
+    //"Depth of Crust": depth
+  //};
+
+  //let legend = L.control({ position: "bottomright" });
+  //legend.onAdd = function() {
+    //let div = L.DomUtil.create("div", "info legend");
+    //let limits = geojson.options.limits;
+    //let colors = geojson.options.colors;
+    //let labels = [];
+//}
